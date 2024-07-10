370816ef8d624100ff55d7b78e174273d1ae2f1a
shiyun33 <chien-s@evidenceb.com>
2024-07-10 14:26:00 +0200

    Done, waiting for ans to queries on Q5,7 in email

--- doctorAnywhere.py
+++ doctorAnywhere.py
@@ -10,7 +10,7 @@ Customers = {
 Orders = {
     'id': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
     'customer_id': [1, 2, 1, 3, 4, 2, 1, 3, 4, 2, 1, 3, 6],
-    'order_date': ['2022-01-01', '2022-01-02','2022-01-03','2022-01-04','2022-02-01','2022-02-021','2022-02-03','2022-02-04','2022-03-01','2022-03-02','2022-03-03','2022-03-04','2022-03-05'],
+    'order_date': ['2022-01-01', '2022-01-02','2022-01-03','2022-01-04','2022-02-01','2022-02-02','2022-02-03','2022-02-04','2022-03-01','2022-03-02','2022-03-03','2022-03-04','2022-03-05'],
     'amount': [100, 200, 50, 300, 150, 100, 75, 200, 250, 50, 125, 175, 60]
 }
 Products = {
@@ -155,16 +155,26 @@ WITH ranked_orders AS (
         o.order_date,
         LEAD(o.order_date) OVER (PARTITION BY o.customer_id ORDER BY o.order_date) AS next_order_date
     FROM orders o
+    WHERE o.customer_id IN (
+        SELECT customer_id
+        FROM orders
+        GROUP BY customer_id 
+        HAVING COUNT(id) > 1
+    )
+      
 )
 SELECT 
     c.id AS customer_id,
     c.name AS customer_name,
-    AVG(julianday(next_order_date) - julianday(order_date)) AS avg_duration_days
+    order_date,
+    next_order_date,
+    ROUND(AVG(julianday(next_order_date) - julianday(order_date))) AS avg_duration_days
 FROM ranked_orders ro
 LEFT JOIN customers c ON ro.customer_id = c.id
-WHERE next_order_date IS NOT NULL  
+WHERE ro.next_order_date IS NOT NULL
 GROUP BY c.name
-HAVING COUNT(*) > 1 ;
+
+ORDER BY customer_id;
 
 '''
 ## Note: DATEDIFF only exists in MySQL, 
