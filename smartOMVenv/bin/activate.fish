d3e1823e56fac0cd41a84a3c7d352f3ca59f8cb7
shiyun33 <chien-s@evidenceb.com>
2024-07-30 09:11:55 +0200

    convert agilealgo3.py to agilealgo.txt for email

--- smartOMVenv/bin/activate.fish
+++ smartOMVenv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/chienshiyun/OA/OA/smartOMVenv"
+set -gx VIRTUAL_ENV "/Users/chienshiyun/Documents/OA/smartOMVenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
